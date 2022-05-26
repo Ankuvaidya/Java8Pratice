c911ca7a794ee97035ed26b29fce8841da80f113
srp <srp>
2022-05-27 00:34:31 +0530

    pratice

--- src/Customer.java
+++ src/Customer.java
@@ -1,13 +1,9 @@
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Customer {
 
 	private String name;
-	private int age ;
-	
+	private int age;
 
 	public Customer(String name, int age) {
 		super();
@@ -15,29 +11,54 @@ public class Customer {
 		this.age = age;
 	}
 
-
 	public Customer() {
 		// TODO Auto-generated constructor stub
 	}
 
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Customer c1= new Customer("anku", 20);
-		Customer c2= new Customer("anku", 20);
-		System.out.println("c1 : "+c1.hashCode());
-		System.out.println("c2 : " +c2.hashCode());
-		HashSet<Customer> cs= new HashSet<Customer>();
+		Customer c1 = new Customer("anku", 20);
+		Customer c2 = new Customer("anku", 20);
+		System.out.println("c1 : " + c1.hashCode());
+		System.out.println("c2 : " + c2.hashCode());
+		HashSet<Customer> cs = new HashSet<Customer>();
 		cs.add(c1);
 		cs.add(c2);
-		System.out.println(cs.size());
-		String s1="ankita";
-		String s2="ankit";
-		System.out.println("s1 : "+s1.hashCode());
-		System.out.println("s2 : "+s2.hashCode());
-		
-		
+		System.out.println("size of hashSet " + cs.size());
 
+		String s1 = "ankita";
+		String s2 = "ankit";
+		System.out.println("s1 : " + s1.hashCode());
+		System.out.println("s2 : " + s2.hashCode());
+
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (age != other.age)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 }
--- src/StreamsPratice/MinNumber.java
+++ src/StreamsPratice/MinNumber.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public class MinNumber {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchFieldException {
 		// TODO Auto-generated method stub
 		List<Integer> i = Arrays.asList( 22, 3, 4, 5, 6, 7);
 		Optional<Integer> r=i.stream().min((e1, e2) -> e1 - e2);
@@ -19,8 +19,9 @@ public class MinNumber {
             return val1.compareTo(val2);
         });
 		System.out.println("min :"+min);
+		List<Integer> i2 = Arrays.asList();
 		
-		Integer max=i.stream().max(Comparator.comparing(Integer::valueOf)).get();
+		Integer max=i2.stream().max(Comparator.comparing(Integer::valueOf)).orElseThrow(NoSuchFieldException:: new);
 		
 			System.out.println("max :" +max);
 		
